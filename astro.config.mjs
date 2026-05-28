db3909428bf617041c2266ecb6ad9178e7e02baf
linyh212 <a100060665@gmail.com>
2026-05-28 23:50:00 +0800

    fix: remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config'
-import githubPages from '@astrojs/github-pages'
 
 export default defineConfig({
-  site: 'https://linyh212.github.io',
-  base: '/linyh212.github.io',
-  integrations: [githubPages()]
+  site: 'https://linyh212.github.io'
 })
